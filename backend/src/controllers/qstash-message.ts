62183643aa52f78cdbb96a633d368d05e7c589e2
biva-labs <bivawebsite@gmail.com>
2025-12-02 14:29:20 +0530

    changes

--- backend/src/controllers/qstash-message.ts
+++ backend/src/controllers/qstash-message.ts
@@ -21,7 +21,7 @@ export const qstash_message = async (c: Context) => {
     const isValid = await receiver.verify({
       body: rawBody,
       signature,
-      url: "https://oscitant-conner-gingelly.ngrok-free.dev/qstash-message",
+      url: "https://biva-bakery.onrender.com/qstash-message",
     });
 
     // console.log(isValid);
@@ -29,7 +29,7 @@ export const qstash_message = async (c: Context) => {
     if (isValid) {
       const userData = body;
       const email = await qstashClient.publishJSON({
-        url: "https://oscitant-conner-gingelly.ngrok-free.dev/send-email",
+        url: "https://biva-bakery.onrender.com/send-email",
         body: {
           userData,
         },
